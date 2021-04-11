061a7531dcb82f47bbc71b42ece017f4b41d229d
XhmikosR <xhmikosr@gmail.com>
2021-04-11 09:48:10 +0300

    Update index.js

--- js/src/util/index.js
+++ js/src/util/index.js
@@ -248,7 +248,8 @@ const execute = callback => {
 const pickFromList = (list, activeElement = null, getNext = true, allowCycle = true) => {
   let index = list.indexOf(activeElement)
 
-  if (index === -1) {// ensure index, if element not exists in list initialize it as first element
+  // ensure index, if element not exists in list initialize it as first element
+  if (index === -1) {
     return list[0]
   }
 
